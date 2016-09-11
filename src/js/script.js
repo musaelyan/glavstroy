670ebece7ca75d10b673dde5eb4a47623c332db9
Сергей Попов <spopov@htmlacademy-2.local>
2016-09-11 13:20:20 +0300

    Js for number field

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-(function() {
-
-})();
+$(function(){
+  $('.js-number').on('click', function(e) {
+    e.preventDefault();
+    var type = $(this).data('type');
+    var field = $(this).parents('.js-number-wrapper').find('input');
+    var rezult;
+    if (type === 0) {
+      rezult = +field.val() - 1;
+      if (rezult < 0) {
+        rezult = 0;
+      }
+    } else if ( type === 1) {
+      rezult = +field.val() + 1;
+    }
+    field.val(rezult);
+  });
+})
